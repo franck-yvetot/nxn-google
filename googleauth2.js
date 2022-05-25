4edd5204bc072f88497efc0e0311fcb9e8d36b14
franck-yvetot <44170458+franck-yvetot@users.noreply.github.com>
2022-05-25 14:11:08 +0200

    jwt impersonnate email

--- googleauth2.js
+++ googleauth2.js
@@ -5,7 +5,7 @@ const { google } = require('googleapis')
 
 var jwt = {};
 
-function getJwt(scopes,keypath) {
+function getJwt(scopes,keypath,email=null) {
   return new Promise(function(resolve, reject)
   {
     keypath = keypath || ".jwt_keys.json";
@@ -15,7 +15,7 @@ function getJwt(scopes,keypath) {
 
     const keys = require(__clientDir+keypath);
 
-    jwt[keypath] = new google.auth.JWT(keys.client_email, null, keys.private_key, scopes);
+    jwt[keypath] = new google.auth.JWT(keys.client_email, null, keys.private_key, scopes,email);
 
     jwt[keypath].authorize((err, response) => {
         if(err)
